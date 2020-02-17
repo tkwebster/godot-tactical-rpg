8d7e708b8ade2dc81e7e72790e8e726fb7d48480
isvpursuit <34354663+isvpursuit@users.noreply.github.com>
2020-02-16 17:17:39 -0800

    controller_support

--- scripts/trpg_camera.gd
+++ scripts/trpg_camera.gd
@@ -11,8 +11,10 @@ var size = 15
 var pos = Vector3(0, 0, 0)
 var arena_size = 15
 var rotat = int(fmod(self.yaw,360.0)/45)
+var size_t = 15
 
 # const:
+const d = .25
 const ROTATION_SPEED = 6.0
 const MOVEMENT_SPEED = 6.0
 
@@ -26,8 +28,7 @@ func logic(var delta, var pawn):
 func _follow_pawn(var delta, var pawn):
 	return
 
-func _rotate_arround(var delta):
-	var d = .25
+func update_rotat():
 	if rotat == 0:
 		w_mod = Vector3(0, 0, -1*d)
 		a_mod = Vector3(-1*d, 0, 0)
@@ -68,19 +69,25 @@ func _rotate_arround(var delta):
 		w_mod = Vector3(-(1/sqrt(2))*d, 0, -(1/sqrt(2))*d)
 		a_mod = Vector3(-(1/sqrt(2))*d, 0, (1/sqrt(2))*d)
 		s_mod = Vector3((1/sqrt(2))*d, 0, (1/sqrt(2))*d)
+
+func _input(event):
 	if Input.is_key_pressed(KEY_W):
+		update_rotat()
 		pos = self.get_translation() + w_mod * MOVEMENT_SPEED
 		pos.x = clamp(pos.x, -arena_size, arena_size)
 		pos.z = clamp(pos.z, -arena_size, arena_size)
 	if Input.is_key_pressed(KEY_A):
+		update_rotat()
 		pos = self.get_translation() + a_mod * MOVEMENT_SPEED
 		pos.x = clamp(pos.x, -arena_size, arena_size)
 		pos.z = clamp(pos.z, -arena_size, arena_size)
 	if Input.is_key_pressed(KEY_S):
+		update_rotat()
 		pos = self.get_translation() + s_mod * MOVEMENT_SPEED
 		pos.x = clamp(pos.x, -arena_size, arena_size)
 		pos.z = clamp(pos.z, -arena_size, arena_size)
 	if Input.is_key_pressed(KEY_D):
+		update_rotat()
 		pos = self.get_translation() + d_mod * MOVEMENT_SPEED
 		pos.x = clamp(pos.x, -arena_size, arena_size)
 		pos.z = clamp(pos.z, -arena_size, arena_size)
@@ -88,6 +95,34 @@ func _rotate_arround(var delta):
 		self.size -= 5
 	if Input.is_action_just_pressed("zoom_out"):
 		self.size += 5
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_WHEEL_UP:
+			self.size -= 1.5
+		if event.button_index == BUTTON_WHEEL_DOWN:
+			self.size += 1.5
+	self.size = clamp(self.size, 10, 25)
+
+func _rotate_arround(var delta):
+	if Input.is_action_pressed("cont_up"):
+		update_rotat()
+		pos = self.get_translation() + w_mod * MOVEMENT_SPEED
+		pos.x = clamp(pos.x, -arena_size, arena_size)
+		pos.z = clamp(pos.z, -arena_size, arena_size)
+	if Input.is_action_pressed("cont_left"):
+		update_rotat()
+		pos = self.get_translation() + a_mod * MOVEMENT_SPEED
+		pos.x = clamp(pos.x, -arena_size, arena_size)
+		pos.z = clamp(pos.z, -arena_size, arena_size)
+	if Input.is_action_pressed("cont_down"):
+		update_rotat()
+		pos = self.get_translation() + s_mod * MOVEMENT_SPEED
+		pos.x = clamp(pos.x, -arena_size, arena_size)
+		pos.z = clamp(pos.z, -arena_size, arena_size)
+	if Input.is_action_pressed("cont_right"):
+		update_rotat()
+		pos = self.get_translation() + d_mod * MOVEMENT_SPEED
+		pos.x = clamp(pos.x, -arena_size, arena_size)
+		pos.z = clamp(pos.z, -arena_size, arena_size)
 	if Input.is_action_just_pressed("ui_left"):
 		self.yaw -= 45.0
 	if Input.is_action_just_pressed("ui_right"):
@@ -103,15 +138,15 @@ func _rotate_arround(var delta):
 		var euler = from.linear_interpolate(to, delta * ROTATION_SPEED)
 		self.set_rotation(euler)
 		rotat = int(fmod(self.yaw,360.0)/45)
-	self.size = clamp(self.size, 10, 25)
-	var size_t = get_child(0).get_size()
+	if self.get_translation() != pos:
+		self.set_translation(self.get_translation().linear_interpolate(pos, delta * MOVEMENT_SPEED))
 	if size_t != size:
 		if size > size_t:
 			self.get_child(0).set_size(get_child(0).get_size()+d)
+			size_t = get_child(0).get_size()
 		else:
 			self.get_child(0).set_size(get_child(0).get_size()-d)
-	if self.get_translation() != pos:
-		self.set_translation(self.get_translation().linear_interpolate(pos, delta * MOVEMENT_SPEED))
+			size_t = get_child(0).get_size()
 
 func _get_selected_tile():
 	if Input.is_action_just_pressed("ui_accept"):
